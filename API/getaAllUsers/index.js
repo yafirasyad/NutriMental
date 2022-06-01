fe78291d3455a5b2c59b1f3aef627c47e961d301
Grinaldy Yafi' Rasyad <46275481+grinaldyafi@users.noreply.github.com>
2022-06-01 15:42:25 +0700

    Delete API directory

--- API/getaAllUsers/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req, GetAllUsers) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-
-  const name = req.query.name || (req.body && req.body.name);
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: GetAllUsers,
-  };
-};
